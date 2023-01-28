061fb72f4e7c534bfc052d77d1841bf76689809a
guanhuai04 <guanhuai04@126.com>
2023-01-28 18:19:51 +0800

    Add JavaDoc

--- src/main/java/hamsteryds/nereusopus/utils/api/NMSUtils.java
+++ src/main/java/hamsteryds/nereusopus/utils/api/NMSUtils.java
@@ -2,28 +2,28 @@ package hamsteryds.nereusopus.utils.api;
 
 import org.bukkit.Bukkit;
 
+/**
+ * <p>NMS工具类</p>
+ */
 public class NMSUtils {
+    /**
+     * NMS版本
+     */
     public static final String version =
             Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
 
+    /**
+     * 获取类
+     *
+     * @param name 类名
+     * @return {@link Class<?>}
+     */
     public static Class<?> getClass(String name) {
         try {
             return Class.forName("net.minecraft.server." + version + "." + name);
         } catch (ClassNotFoundException e) {
             try {
                 return Class.forName("org.bukkit.craftbukkit." + version + "." + name);
-            } catch (ClassNotFoundException e1) {
-                return null;
-            }
-        }
-    }
-
-    public static Class<?> getClassWithPackage(String name) {
-        try {
-            return Class.forName(name);
-        } catch (ClassNotFoundException e) {
-            try {
-                return Class.forName(name);
             } catch (ClassNotFoundException e1) {
                 return null;
             }
