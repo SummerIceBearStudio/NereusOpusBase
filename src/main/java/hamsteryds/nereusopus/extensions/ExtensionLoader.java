81f53faa64f78194ff98b7daa9d8f20adb728614
HamsterYDS <1294243258@qq.com>
2023-01-29 21:21:47 +0800

    new displayutils

--- src/main/java/hamsteryds/nereusopus/extensions/ExtensionLoader.java
+++ src/main/java/hamsteryds/nereusopus/extensions/ExtensionLoader.java
@@ -7,19 +7,30 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 public class ExtensionLoader {
+    public static File directory_1 = new File(NereusOpus.plugin.getDataFolder() + "/extensions/enchants");
+    public static File directory_2 = new File(NereusOpus.plugin.getDataFolder() + "/extensions/functions");
+
     public static void initialize() {
-        File directory = new File(NereusOpus.plugin.getDataFolder() + "/extensions/");
-        if (!directory.exists())
-            directory.mkdirs();
-        if (directory.listFiles() == null) {
+        if (!directory_1.exists())
+            directory_1.mkdirs();
+        if (!directory_2.exists())
+            directory_2.mkdirs();
+
+        loadEnchantExtensions();
+    }
+
+    public static void loadEnchantExtensions() {
+        if (directory_1.listFiles() == null) {
             return;
         }
-        for (File file : directory.listFiles()) {
+        for (File file : directory_1.listFiles()) {
             try {
                 URL url = new URL("file:" + file.getAbsolutePath());
                 URLClassLoader classLoader = new URLClassLoader(new URL[]{url}, Thread.currentThread().getContextClassLoader());
-                Class clazz = classLoader.loadClass("nereusopus.extension.Main");
-                clazz.newInstance();
+//                System.out.println(List.of(classLoader.get));
+//
+//                Class clazz = classLoader.loadClass("nereusopus.extension.Main");
+//                clazz.newInstance();
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
