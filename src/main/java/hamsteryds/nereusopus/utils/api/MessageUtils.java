e9e1222f6dfd4e0883471b2b095c15c8c60f6eeb
guanhuai04 <guanhuai04@126.com>
2023-01-29 23:05:39 +0800

    Add JavaDoc

--- src/main/java/hamsteryds/nereusopus/utils/api/MessageUtils.java
+++ src/main/java/hamsteryds/nereusopus/utils/api/MessageUtils.java
@@ -6,33 +6,64 @@ import org.bukkit.entity.Player;
 
 import java.util.*;
 
+/**
+ * <p>消息工具类</p>
+ */
 public class MessageUtils {
+    /**
+     * 玩家消息列表
+     */
     private static final Map<UUID, List<String>> messages = new HashMap<>();
 
-    public static void sendMessage(Player p, String message) {
-        p.sendMessage(message);
+    /**
+     * 向在线玩家发送消息
+     *
+     * @param player       在线玩家
+     * @param message 消息
+     */
+    public static void sendMessage(Player player, String message) {
+        player.sendMessage(message);
     }
 
-    public static void sendMessage(OfflinePlayer p, String message) {
-        if (p.isOnline()) {
-            ((Player) p).sendMessage(message);
+    /**
+     * 向玩家发送消息
+     *
+     * @param player       玩家
+     * @param message 消息
+     *
+     * @see <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/OfflinePlayer.html">{@code OfflinePlayer}</a>
+     */
+    public static void sendMessage(OfflinePlayer player, String message) {
+        if (player.isOnline()) {
+            ((Player) player).sendMessage(message);
         } else {
-            messages.putIfAbsent(p.getUniqueId(), new ArrayList<>());
-            messages.get(p.getUniqueId()).add(message);
+            messages.putIfAbsent(player.getUniqueId(), new ArrayList<>());
+            messages.get(player.getUniqueId()).add(message);
         }
     }
 
+    /**
+     * 玩家发送消息
+     *
+     * @param uuid    玩家UUID
+     * @param message 消息
+     */
     public static void sendMessage(UUID uuid, String message) {
         sendMessage(Bukkit.getOfflinePlayer(uuid), message);
     }
 
-    public static void sync(Player p) {
-        if (!messages.containsKey(p.getUniqueId())) {
+    /**
+     * 同步玩家消息
+     *
+     * @param player 玩家
+     */
+    public static void sync(Player player) {
+        if (!messages.containsKey(player.getUniqueId())) {
             return;
         }
-        for (String message : messages.get(p.getUniqueId())) {
-            p.sendMessage(message);
+        for (String message : messages.get(player.getUniqueId())) {
+            player.sendMessage(message);
         }
-        messages.remove(p.getUniqueId());
+        messages.remove(player.getUniqueId());
     }
 }
