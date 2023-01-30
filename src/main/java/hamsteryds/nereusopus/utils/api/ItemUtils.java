90894bc000b2150ea140b6164678cd950b378df8
MartinGuan04 <guanhuai1006@126.com>
2023-01-30 17:41:00 +0800

    Add JavaDoc

--- src/main/java/hamsteryds/nereusopus/utils/api/ItemUtils.java
+++ src/main/java/hamsteryds/nereusopus/utils/api/ItemUtils.java
@@ -22,10 +22,25 @@ import org.jetbrains.annotations.NotNull;
 import java.lang.reflect.Field;
 import java.util.*;
 
+/**
+ * 物品工具类
+ */
 public class ItemUtils {
+    /**
+     * 附魔<a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/NamespacedKey.html">{@code NamespacedKey}</a>
+     */
     public static final NamespacedKey enchantKey;
+    /**
+     * 附魔信息<a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/NamespacedKey.html">{@code NamespacedKey}</a>
+     */
     public static final NamespacedKey enchantsInfoKey;
+    /**
+     * 配置参数<a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/NamespacedKey.html">{@code NamespacedKey}</a>
+     */
     public static final NamespacedKey configParamKey;
+    /**
+     * 配置参数<a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/NamespacedKey.html">{@code NamespacedKey}</a>
+     */
     public static final NamespacedKey configValueKey;
     private static Field profileField;
 
@@ -43,16 +58,41 @@ public class ItemUtils {
         configValueKey = new NamespacedKey("summericebearstore", "configvalue");
     }
 
+    /**
+     * 创建物品
+     *
+     * @param type        物品类型
+     * @param displayName 显示名称
+     * @param lore        物品的lore
+     * @return 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     public static ItemStack make(Material type, String displayName, String... lore) {
         return make(type, displayName, Arrays.asList(lore));
     }
 
+    /**
+     * 创建物品
+     *
+     * @param type        物品类型
+     * @param amount      物品数量
+     * @param displayName 显示名称
+     * @param lore        物品的lore
+     * @return 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     public static ItemStack make(Material type, int amount, String displayName, String... lore) {
         ItemStack item = make(type, displayName, Arrays.asList(lore));
         item.setAmount(amount);
         return item;
     }
 
+    /**
+     * 创建物品
+     *
+     * @param type        物品类型
+     * @param displayName 显示名称
+     * @param lore        物品的lore
+     * @return 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     public static ItemStack make(Material type, String displayName, List<String> lore) {
         ItemStack item = new ItemStack(type);
         ItemMeta meta = item.getItemMeta();
@@ -66,6 +106,13 @@ public class ItemUtils {
         return item;
     }
 
+    /**
+     * 增加耐久度
+     *
+     * @param item    物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @param percent 耐久度百分比
+     * @return 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     public static ItemStack addDurability(ItemStack item, double percent) {
         if (percent < 1 && percent > 0) {
             item.setDurability((short) Math.max(item.getDurability() - item.getType().getMaxDurability() * percent, 0));
@@ -75,6 +122,13 @@ public class ItemUtils {
         return item;
     }
 
+    /**
+     * 增加耐久度
+     *
+     * @param item    物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @param point   增加的耐久度
+     * @return 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     public static ItemStack addDurability(ItemStack item, short point) {
         if (item == null || !isDamageable(item)) {
             return item;
@@ -92,10 +146,22 @@ public class ItemUtils {
         return item;
     }
 
+    /**
+     * 判断物品是否损坏（耐久度为0）
+     *
+     * @param itemStack 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @return boolean 是否损坏
+     */
     public static boolean isDamageable(ItemStack itemStack) {
         return itemStack.getType().getMaxDurability() > 0;
     }
 
+    /**
+     * 创建附魔书
+     *
+     * @param enchants 附魔
+     * @return 附魔书的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     @SafeVarargs
     public static ItemStack makeEnchantedBook(Pair<Enchantment, Integer>... enchants) {
         ItemStack item = make(Material.ENCHANTED_BOOK, "");
@@ -107,6 +173,12 @@ public class ItemUtils {
         return item;
     }
 
+    /**
+     * 判断是否可以磨砂
+     *
+     * @param item 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @return boolean 是否可以磨砂
+     */
     public static boolean grindstoneable(ItemStack item) {
         if (item == null) {
             return true;
@@ -125,6 +197,13 @@ public class ItemUtils {
         return true;
     }
 
+    /**
+     * 创建头颅
+     *
+     * @param item       物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @param headBase64 头颅的base64编码
+     * @return 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     public static ItemStack setSkull(ItemStack item, String headBase64) {
         if (item.getItemMeta() instanceof SkullMeta meta) {
             GameProfile profile = new GameProfile(UUID.fromString("036fd219-4618-3c50-92ab-e687de3eb47a"), "HamsterYDS");
@@ -139,12 +218,29 @@ public class ItemUtils {
         return item;
     }
 
+    /**
+     * <p>通过字符串创建物品</p>
+     * 具体规则请参阅配置文件下的{@code display.item}属性
+     *
+     * @param string 字符串
+     * @return 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     */
     public static ItemStack fromString(String string) {
         String[] splited = string.replace("&", "§").split(";");
         int length = splited.length;
         return make(Material.valueOf(splited[0]), length >= 2 ? splited[1] : "", length >= 3 ? splited[2].split(",") : new String[0]);
     }
 
+    /**
+     * 获得物品的附魔
+     *
+     * @param item 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @return <p>{@link Map}&lt;{@link Enchantment}, {@link Integer}&gt; 附魔</p>
+     *         <ul>
+     *             <li>{@link Enchantment} - 附魔</li>
+     *             <li>{@link Integer} - 等级</li>
+     *         </ul>
+     */
     public static Map<Enchantment, Integer> getEnchants(ItemStack item) {
         if (item == null) {
             return new HashMap<>();
@@ -156,6 +252,12 @@ public class ItemUtils {
         }
     }
 
+    /**
+     * 设置物品的附魔
+     *
+     * @param item          物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @param addedEnchants 附魔
+     */
     public static void setEnchants(ItemStack item, Map<Enchantment, Integer> addedEnchants) {
         Map<Enchantment, Integer> enchants = getEnchants(item);
         if (item != null) {
@@ -183,6 +285,12 @@ public class ItemUtils {
         }
     }
 
+    /**
+     * 得到物品的lore
+     *
+     * @param item 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @return {@link List}&lt;{@link String}&gt; lore
+     */
     public static List<String> getLore(ItemStack item) {
         if (item != null) {
             if (item.getItemMeta() != null) {
@@ -194,6 +302,12 @@ public class ItemUtils {
         return new ArrayList<>();
     }
 
+    /**
+     * 得到物品的显示名称
+     *
+     * @param item 物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @return {@link String} 显示名称
+     */
     public static String getDisplayName(ItemStack item) {
         if (item != null) {
             if (item.getItemMeta() != null) {
@@ -205,14 +319,35 @@ public class ItemUtils {
         return "";
     }
 
+    /**
+     * 获得物品的附魔等级
+     *
+     * @param item  物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @param clazz clazz
+     * @return int 附魔等级
+     */
     public static int getEnchantLevel(ItemStack item, Class<? extends AbstractEnchantment> clazz) {
         return item.getEnchantmentLevel(EnchantmentUtils.fromID(clazz.getSimpleName()));
     }
 
+    /**
+     * 获得物品的附魔等级
+     *
+     * @param item  物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @param id    附魔的id
+     * @return int 附魔等级
+     */
     public static int getEnchantLevel(ItemStack item, String id) {
         return item.getEnchantmentLevel(EnchantmentUtils.fromID(id));
     }
 
+    /**
+     * 增加物品耐耐久度
+     *
+     * @param item  物品的 <a href="https://hub.spigotmc.org/javadocs/spigot/org/bukkit/inventory/ItemStack.html">{@code ItemStack}</a>
+     * @param added 耐久度
+     * @return {@link ItemStack}
+     */
     public static ItemStack addDurability(ItemStack item, int added) {
         ItemMeta meta = item.getItemMeta();
         if (meta == null) {
@@ -232,6 +367,12 @@ public class ItemUtils {
         return item;
     }
 
+    /**
+     * 获得材质
+     *
+     * @param name 材质名称
+     * @return {@link Material} 材质
+     */
     public static Material getMaterial(String name) {
         try {
             return Material.valueOf(name.toUpperCase(Locale.ROOT));
--- src/main/java/hamsteryds/nereusopus/utils/api/MechanismUtils.java
+++ src/main/java/hamsteryds/nereusopus/utils/api/MechanismUtils.java
@@ -15,10 +15,33 @@ import org.bukkit.event.block.BlockBreakEvent;
 import java.util.HashMap;
 import java.util.UUID;
 
+/**
+ * 机制工具类
+ */
 public class MechanismUtils {
+    /**
+     * 玩家冷却时间戳
+     */
     public static final HashMap<UUID, HashMap<String, Long>> usedStamps = new HashMap<>();
+    /**
+     * <p>方块权限缓存</p>
+     * <ul>
+     *     <li>{@link UUID} - 玩家UUID</li>
+     *     <li>{@link String} - 坐标</li>
+     *     <li>{@link Boolean} - 是否有权限</li>
+     * </ul>
+     */
     public static HashMap<UUID, HashMap<String, Boolean>> cache = new HashMap<>();
 
+    /**
+     * 检查技能冷却
+     *
+     * @param player 玩家
+     * @param key    技能名字
+     * @param cd     技能cd
+     * @param info   是否向玩家发送冷却信息
+     * @return boolean 技能是否已冷却
+     */
     public static boolean checkCooldown(Player player, String key, double cd, boolean info) {
         UUID uuid = player.getUniqueId();
         double rate = 1.0;
@@ -53,6 +76,12 @@ public class MechanismUtils {
         return true;
     }
 
+    /**
+     * 添加技能冷却
+     *
+     * @param player 玩家
+     * @param key    技能名字
+     */
     public static void addCooldown(Player player, String key) {
         UUID uuid = player.getUniqueId();
         if (!usedStamps.containsKey(uuid)) {
@@ -61,10 +90,24 @@ public class MechanismUtils {
         usedStamps.get(uuid).put(key, System.currentTimeMillis());
     }
 
+    /**
+     * 判断对于有触发几率的附魔是否能触发
+     *
+     * @param enchant 附魔
+     * @param level   附魔等级
+     * @return boolean 是否能触发
+     */
     public static boolean checkPercent(CustomEnchantment enchant, int level) {
         return Math.random() * 100 <= enchant.getValue("chance", level, 100 + "");
     }
 
+    /**
+     * 判断对于随攻击冷却的附魔是否能触发
+     *
+     * @param enchant 附魔
+     * @param entity  触发附魔的实体
+     * @return boolean 是否能触发
+     */
     public static boolean checkRequireFull(CustomEnchantment enchant, LivingEntity entity) {
         if (!(entity instanceof Player player)) {
             return true;
@@ -75,6 +118,14 @@ public class MechanismUtils {
         return true;
     }
 
+    /**
+     * 检查方块权限
+     *
+     * @param enchant  附魔
+     * @param block    方块
+     * @param creature 触发附魔的生物
+     * @return boolean 是否有权限
+     */
     public static boolean checkPermission(CustomEnchantment enchant, Block block, LivingEntity creature, boolean... flag) {
         if (creature instanceof Player player) {
             if (enchant.getBool("permission-check", true)) {
@@ -100,15 +151,28 @@ public class MechanismUtils {
         return true;
     }
 
+    /**
+     * 检查攻击者是否在掉落过程中
+     *
+     * @param attacker 攻击者
+     * @return boolean 是否在掉落过程中
+     */
     public static boolean checkCritical(Entity attacker) {
         return attacker.getFallDistance() > 0 && !attacker.isOnGround();
     }
 
-    public static boolean checkShiftIgnored(EventExecutor executor, LivingEntity entity) {
+    /**
+     * 检查附魔下蹲时是否触发
+     *
+     * @param enchant 附魔
+     * @param entity   实体
+     * @return boolean 是否能触发
+     */
+    public static boolean checkShiftIgnored(CustomEnchantment enchant, LivingEntity entity) {
         if (!(entity instanceof Player player)) {
             return true;
         }
-        if (executor.getBool("shift-ignored", false)) {
+        if (enchant.getBool("shift-ignored", false)) {
             return !player.isSneaking();
         }
         return true;
